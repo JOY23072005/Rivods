6fb5f4034ef8529bc8ebb77f09d28bed8dcab289
JOY23072005 <hansjoydeep@gmail.com>
2026-08-22 17:33:17 +0530

    refractor(challenge): challenge image const url changed

--- Backend/src/lib/challenges/challengeImage.js
+++ Backend/src/lib/challenges/challengeImage.js
@@ -1,6 +1,6 @@
 export const CHALLENGE_IMAGES = {
-  daily: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787397017/Screenshot_2026-08-22_at_4.30.02_PM_nmj811.png",
-  weekly: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787397019/Screenshot_2026-08-22_at_4.29.51_PM_bjkpbe.png",
-  monthly: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787397016/Screenshot_2026-08-22_at_4.30.11_PM_eep8zm.png",
-  custom: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787397015/Screenshot_2026-08-22_at_4.30.22_PM_pkvbx0.png",
+  daily: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787400053/Screenshot_2026-08-22_at_4.49.13_PM_lxe9kq.png",
+  weekly: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787400055/Screenshot_2026-08-22_at_4.49.02_PM_lh1rfa.png",
+  monthly: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787400051/Screenshot_2026-08-22_at_4.49.23_PM_qc88sd.png",
+  custom: "https://res.cloudinary.com/dgwx34lqc/image/upload/v1787400049/Screenshot_2026-08-22_at_4.49.32_PM_jwcnp4.png",
 };
